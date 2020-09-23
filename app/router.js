dc82c505da0462c08c5dcc681080a313dbc6394e
chenyitao <chenyitao@zhuanzhuan.com>
2020-09-23 18:01:37 +0800

    feat: 添加isOpt路由

--- app/router.js
+++ app/router.js
@@ -7,5 +7,6 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/startGame', controller.home.startGame);
+  router.get('/isOpt', controller.home.isOpt);
   router.get('/resetUsers', controller.home.resetUsers);
 };
